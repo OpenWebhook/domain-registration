7c1811c44d12a6b94a319201e1f0fb701a3bda20
Sammy Teillet <sammy.teillet@gmail.com>
2023-03-30 18:14:10 +0200

    feat: create DNS record

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 type Sender struct {
-	Login string `json:"login"`
+	Login string `json:"login" binding:"required"`
 }
 
 type JSONGithubWebhook struct {
@@ -23,6 +23,14 @@ type JSONGithubWebhook struct {
 
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	dnsRecordContent := os.Getenv("DNS_RECORD_CONTENT")
+	if dnsRecordContent == "" {
+		log.Fatal("$DNS_RECORD_CONTENT must be set")
+	}
 
 	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
 	if err != nil {
@@ -32,10 +40,6 @@ func main() {
 	cloudflareZoneID, err := api.ZoneIDByName(os.Getenv("CLOUDFLARE_DOMAIN"))
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
 	}
 
 	router := gin.New()
@@ -56,19 +60,28 @@ func main() {
 			c.JSON(http.StatusOK, gin.H{"message": "only created events are accepted"})
 			return
 		}
-		log.Println(json.Sender.Login)
 
-		records, _, err := api.ListDNSRecords(context.Background(), cloudflare.ZoneIdentifier(cloudflareZoneID), cloudflare.ListDNSRecordsParams{})
+		name := "*." + json.Sender.Login + ".github"
+
+		fmt.Println("Attempting to create dns record for " + name)
+
+		var createDNSRecordParams = cloudflare.CreateDNSRecordParams{
+			Content: dnsRecordContent,
+			Name:    name,
+			Type:    "CNAME",
+			Comment: "Github user wildcard",
+			TTL:     3600,
+		}
+
+		record, err := api.CreateDNSRecord(context.Background(), cloudflare.ZoneIdentifier(cloudflareZoneID), createDNSRecordParams)
 		if err != nil {
 			fmt.Println(err)
+			c.JSON(http.StatusCreated, gin.H{"message": "star gazer not created"})
 			return
 		}
+		fmt.Println(record)
 
-		for _, r := range records {
-			fmt.Printf("%s: %s\n", r.Name, r.Content)
-		}
-
-		c.JSON(http.StatusCreated, gin.H{"message": "star gazer created", "numberOfRecords": len(records)})
+		c.JSON(http.StatusCreated, gin.H{"message": "star gazer created"})
 
 	})
 
