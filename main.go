1c24d98f87e3fe331d723c4b2b980fa627bb6598
Sammy Teillet <sammy.teillet@gmail.com>
2023-03-30 16:56:10 +0200

    feat: handle github webhook

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+type Sender struct {
+	Login string `json:"login"`
+}
+
+type JSONGithubWebhook struct {
+	Sender Sender `json:"sender" binding:"required"`
+	Action string `json:"action" binding:"required"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -19,8 +28,18 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"hello": "world"})
+	router.POST("/", func(c *gin.Context) {
+		var json JSONGithubWebhook
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.Action != "created" {
+			c.JSON(http.StatusNotAcceptable, gin.H{"message": "only created events are accepted"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "star gazer created"})
 	})
 
 	router.Run(":" + port)
